a4f070ca6345ce26f364ffefea3aca18b839deb0
FatineBts <fatinealj@hotmail.fr>
2019-01-07 01:34:27 +0100

    Delete __init__.py

--- scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-##################################################################
-#Polytech Sorbonne - année 2018/2019
-#Réalisé par : Fatine Bentires Alj et Alexia Zounias-Sirabella
-#Cours de maillage des éléments finis de Bertrand Thierry
-##################################################################
