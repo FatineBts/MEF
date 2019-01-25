1fe7ee95547cfb2454349a467e918c3dfdd7ce8b
FatineBts <fatinebentires@gmail.com>
2019-01-25 22:03:25 +0100

    un peu plus logique

--- Scripts/matrices.py
+++ Scripts/matrices.py
@@ -52,8 +52,8 @@ class Matrice:
 		self.Nodes = Nodes
 		self.Elements = Elements
 		self.Nombre_Elements = Nombre_Elements
-		self.alpha = 0.9
-		self.k = np.pi
+		self.alpha = np.pi
+		self.k = 2*np.pi
 
 	def f(self,x):
 		return 0
@@ -221,16 +221,16 @@ class Matrice:
 			if(e[1]==1 and e[3]==2): #bord intérieur
 				for i in range(0,2):
 					#sur toutes les lignes et colonnes on met 0 
-					A[e[len(e)-2+i]-1,:] = 0 #on met la ligne à 0 
-					A[:,e[len(e)-2+i]-1] = 0  #on met la colonne à 0
+					A[e[len(e)-3+i]-1,:] = 0 #on met la ligne à 0 
+					A[:,e[len(e)-3+i]-1] = 0  #on met la colonne à 0
 					#sur la diagonale on met 1 
-					A[e[len(e)-2+i]-1,e[len(e)-2+i]-1] = 1
+					A[e[len(e)-3+i]-1,e[len(e)-3+i]-1] = 1
+					#print("e",e)
+					#print("truc",e[len(e)-3+i])
 
 		ecriture = Ecriture("A_dirichlet.csv")
 		ecriture.ecriture(A)
-		#print(Masse.shape)
-		#print(Rigidite.shape)
-		#print(A.shape)
+
 		return A
 
 	def calcul_membre_droite(self):
@@ -243,11 +243,13 @@ class Matrice:
 		#Dirichlet 
 		for e in self.Elements: 
 			if(e[1]==1 and e[3]==2): #alors il s'agit d'un segment et on est sur le bord intérieur
-				p1 = e[len(e)-2] #on a que deux points 
-				p2 = e[len(e)-1]
+				print(e)
+				p1 = e[len(e)-3] #on a que deux points 
+				p2 = e[len(e)-2]
 				#liste des coordonnées pour chaque point (pas z car on est en 2 D)
 				s1 = self.Nodes[p1-1] #sommet pour le triangle e 
 				s2 = self.Nodes[p2-1] 
+
 				second_membre[p1-1] = -self.u_inc(s1)
 				second_membre[p2-1] = -self.u_inc(s2)
 
@@ -258,9 +260,9 @@ class Matrice:
 		#print(second_membre.shape)
 		return second_membre
 
-	def resolution_systeme(self,A,membre_droite_dirichlet): 
+	def resolution_systeme(self,A,membre_droite): 
 		#toarray permet de mettre en deux dimensions
-		x = np.linalg.solve(A,membre_droite_dirichlet)
+		x = np.linalg.solve(A,membre_droite)
 		ecriture = Ecriture("resolution_systeme.csv")
 		ecriture.ecriture(x)
 		return x
